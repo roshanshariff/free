19448883d4202fd77646a3aabffb3fbc67a451f8
Roshan Shariff <roshan.shariff@ualberta.ca>
2013-04-01 22:54:25 -0600

    Added common instances for Control.Monad.Trans.Free.Church.FT

--- src/Control/Monad/Trans/Free/Church.hs
+++ src/Control/Monad/Trans/Free/Church.hs
@@ -16,10 +16,12 @@ module Control.Monad.Trans.Free.Church
 
 import Prelude hiding (mapM, sequence)
 import Control.Applicative
-import Control.Monad (join, (<=<))
+import Control.Monad (MonadPlus(..), join, (<=<))
+import Control.Monad.IO.Class
 import Control.Monad.Free.Class
 import Control.Monad.Trans.Free
 import Control.Monad.Trans.Class
+import Data.Foldable
 import Data.Traversable
 
 newtype FT f m a = FT { runFT :: forall r. (a -> m r) -> (f (m r) -> m r) -> m r }
@@ -30,7 +32,7 @@ instance Functor (FT f m) where
 instance Applicative (FT f m) where
   pure a = FT $ \kp _ -> kp a
   FT f <*> FT g = FT $ \kp kf -> f (\a -> g (\b -> kp $ a b) kf) kf
-  
+
 instance Monad (FT f m) where
   return a = FT $ \kp _ -> kp a
   FT m >>= f = FT $ \kp kf -> m (\a -> runFT (f a) kp kf) kf
@@ -41,6 +43,25 @@ instance (Functor f) => MonadFree f (FT f m) where
 instance MonadTrans (FT f) where
   lift a = FT $ \kp _ -> a >>= kp
 
+instance (MonadPlus m) => MonadPlus (FT f m) where
+  mzero = FT $ \_ _ -> mzero
+  mplus (FT f) (FT g) = FT $ \kp kf -> mplus (f kp kf) (g kp kf)
+
+instance (MonadPlus m) => Alternative (FT f m) where
+  empty = mzero
+  (<|>) = mplus
+
+instance (MonadIO m) => MonadIO (FT f m) where
+  liftIO = lift . liftIO
+  
+instance (Foldable f, Foldable m, Monad m) => Foldable (FT f m) where
+  foldMap f (FT g) = fold $ g (return . f) (return . foldMap fold)
+
+instance (Traversable f, Traversable m, Monad m) => Traversable (FT f m) where
+  sequenceA (FT g) = mAlg $ g (return . (return <$>)) (return . fmAlg)
+    where mAlg = fmap (join . lift) . sequenceA -- m (h (FT m b)) -> h (FT m b)
+          fmAlg = fmap wrap . traverse mAlg -- f (m (h (FT m b))) -> h (FT m b)
+
 hoistFT :: (Functor f, Monad m, Monad n) => (forall a. m a -> n a) -> FT f m b -> FT f n b
 hoistFT mu (FT f) = FT $ \kp kf -> join . mu . f (return . kp) $ return . kf . fmap (join . mu)
 
@@ -48,7 +69,7 @@ transFT :: (forall a. f a -> g a) -> FT f m b -> FT g m b
 transFT mu (FT f) = FT $ \kp kf -> f kp (kf . mu)
 
 cataFT :: (a -> m b) -> (f (m b) -> m b) -> FT f m a -> m b
-cataFT kp kf ft = runFT ft kp kf
+cataFT kp kf (FT f) = f kp kf
 
 foldFT :: (Traversable f, Monad m) => (a -> m b) -> (f b -> m b) -> FT f m a -> m b
 foldFT kp kf = cataFT kp $ kf <=< sequence
